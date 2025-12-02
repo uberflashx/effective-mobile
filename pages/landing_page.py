7965eab5f519ee49ef77289cf7fbb73b2dc16fc7
uberflashx <uberflashx@gmail.com>
2025-12-03 01:35:32 +0300

    Adding locators and methods to redirections test

--- pages/landing_page.py
+++ pages/landing_page.py
@@ -1,11 +1,25 @@
 from pages.base_page import BasePage
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 class LandingPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
         self.about_link = page.locator('//a[contains(@href, "about")]')
+        self.about_block = page.locator('section#about')
+        self.vacancies_link = page.locator('//a[contains(@href, "specializations")]')
+        self.reviews_link = page.locator('//a[contains(@href, "testimonials")]')
+        self.contacts_link = page.locator('//a[contains(@href, "contact")]')
 
-    def click_about_link(self):
+        self.outstaff_link = page.locator('//a[contains(@href, "services")]').nth(0)
+        self.outstaff_block = page.locator('section#services')
+        self.employment_link = page.locator('//a[contains(@href, "services")]').nth(1)
+        self.callback_link = page.locator('//a[contains(@href, "contact")]')
+
+    def outstaff_link_redirect(self):
+        self.outstaff_link.click()
+        expect(self.outstaff_block).to_be_visible()
+
+    def about_link_redirect(self):
         self.about_link.click()
+        expect(self.about_block).to_be_visible()
--- tests/test_redirections.py
+++ tests/test_redirections.py
@@ -5,4 +5,5 @@ from pages.landing_page import LandingPage
 def test_footer_link_redirections(chromium_page: Page):
     landing_page = LandingPage(page=chromium_page)
     landing_page.visit('https://www.effective-mobile.ru')
-    landing_page.click_about_link()
+    landing_page.outstaff_link_redirect()
+    landing_page.about_link_redirect()
